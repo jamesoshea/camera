d7bfd40679f8706d5149830f0a668ec06f2c9aac
James O'Shea <jamesoshea89@hotmail.com>
2017-11-26 17:06:35 +0100

    initial commit

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,32 @@
+<template>
+	<h1 v-text="headerText"></h1>
+</template>
+
+<script>
+export default {
+	name: 'HeaderBar',
+	data() {
+		return {
+			headerText: 'Camera App'
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import './../assets/variables.scss';
+
+h1 {
+	background-color: $light-color;
+	color: $light-color;
+	margin: 0;
+	font-size: 8vh;
+	height: 10vh;
+}
+
+h1 {
+  text-align: center;
+}
+
+</style>
---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase'
+
+const config = {
+	apiKey: "AIzaSyCPQuSO-sRAMajUT-L4Pgl-3jdc8caWmew",
+	authDomain: "mothergirth-327a5.firebaseapp.com",
+	databaseURL: "https://mothergirth-327a5.firebaseio.com",
+	projectId: "mothergirth-327a5",
+	storageBucket: "mothergirth-327a5.appspot.com",
+	messagingSenderId: "807456386997"
+};
+
+
+firebase.initializeApp(config)
+const storage =  firebase.storage()
+const database = firebase.database()
+
+export {
+  database,
+  storage
+}
