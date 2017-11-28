4cf7d76f0de6f1fa01c5466e50269c5e2215e037
James O'Shea <jamesoshea89@hotmail.com>
2017-11-28 19:51:42 +0100

    vue router added

---
+++ src/components/Gallery.vue
@@ -0,0 +1,25 @@
+<template>
+	<ul>
+		<li v-for="photo in photos"></li>
+	</ul>
+</template>
+
+<script>
+
+import { storage } from './../config/firebase'
+
+export default {
+	mounted () {
+		this.data = storage.ref().child(`images`)
+	},
+	data() {
+		return {
+			photos: [],
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Camera from './components/Camera.vue'
+import Gallery from './components/Gallery.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Camera },
+    { path: '/gallery', component: Gallery }
+  ],
+  mode: 'history',
+})
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router,
 })
