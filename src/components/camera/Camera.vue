9a63581d1a79ff09c8d5d381ea9c791024c0535e
James O'Shea <jamesoshea89@hotmail.com>
2017-12-03 21:30:41 +0100

    cancel photo function

--- src/components/camera/Camera.vue
+++ src/components/camera/Camera.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="camera-view">
     <div v-if="showOverlay" class="camera-overlay">
+      <div class="cancel-button-container" @click="cancelPhoto">
+        <svg viewBox="0 0 45 45" xml:space="preserve">
+          <g>
+            <path d="M14.1,11.3c-0.2-0.2-0.2-0.5,0-0.7l7.5-7.5c0.2-0.2,0.3-0.5,0.3-0.7s-0.1-0.5-0.3-0.7l-1.4-1.4C20,0.1,19.7,0,19.5,0  c-0.3,0-0.5,0.1-0.7,0.3l-7.5,7.5c-0.2,0.2-0.5,0.2-0.7,0L3.1,0.3C2.9,0.1,2.6,0,2.4,0S1.9,0.1,1.7,0.3L0.3,1.7C0.1,1.9,0,2.2,0,2.4  s0.1,0.5,0.3,0.7l7.5,7.5c0.2,0.2,0.2,0.5,0,0.7l-7.5,7.5C0.1,19,0,19.3,0,19.5s0.1,0.5,0.3,0.7l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3  s0.5-0.1,0.7-0.3l7.5-7.5c0.2-0.2,0.5-0.2,0.7,0l7.5,7.5c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.2-0.2,0.3-0.5,0.3-0.7  s-0.1-0.5-0.3-0.7L14.1,11.3z" />
+          </g>
+        </svg>
+      </div>
       <div>
         <input class="caption-input" type="text" v-model="caption">
       </div>
@@ -51,6 +58,16 @@ export default {
     }
   },
   methods: {
+    cancelPhoto() {
+      this.showOverlay = false
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(mediaStream => {
+          this.mediaStream = mediaStream
+          this.$refs.video.srcObject = mediaStream
+          this.$refs.video.play()
+        })
+        .catch(error => console.log(error))
+    },
     takePhoto() {
       const mediaStreamTrack = this.mediaStream.getVideoTracks()[0]
       const imageCapture = new window.ImageCapture(mediaStreamTrack)
@@ -71,7 +88,6 @@ export default {
               self.showOverlay = false;
               self.$router.push('/gallery')
             })
-            .catch(() => console.log('photo set error'))
         })
     }
   }
@@ -104,6 +120,20 @@ export default {
 
 .camera-stream {
     width: 100%;
+}
+
+.cancel-button-container {
+	text-align: center;
+	position: fixed;
+  margin: 0 auto;
+  left: 85vw;
+  top: 22.5vw;
+  width: 10vh;
+  height: 10vh;
+  border-radius: 5vh;
+  fill: $light-color;
+  cursor: pointer;
+  z-index: 10;
 }
 
 .caption-input {
